1c1f4032719bc41ad4a97d94e839eca1add2ff02
chamseddine <chamseddine.benhamed@ensi-uma.tn>
2020-10-21 14:03:47 +0200

    checkstyle fixes
    Signed-off-by: chamseddine <chamseddine.benhamed@ensi-uma.tn>

--- src/main/java/org/gridsuite/merge/orchestrator/server/IgmQualityCheckService.java
+++ src/main/java/org/gridsuite/merge/orchestrator/server/IgmQualityCheckService.java
@@ -12,8 +12,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.powsybl.commons.PowsyblException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
