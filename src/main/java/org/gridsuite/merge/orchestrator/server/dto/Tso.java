4882f5c1fe1e64f14eeaab72e305bd5462a63e58
chamseddine <chamseddine.benhamed@ensi-uma.tn>
2021-01-05 13:14:50 +0100

    update Tso
    Signed-off-by: chamseddine <chamseddine.benhamed@ensi-uma.tn>

--- src/main/java/org/gridsuite/merge/orchestrator/server/dto/Tso.java
+++ src/main/java/org/gridsuite/merge/orchestrator/server/dto/Tso.java
@@ -8,25 +8,15 @@ package org.gridsuite.merge.orchestrator.server.dto;
 
 import io.swagger.annotations.ApiModel;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.ToString;
-import org.gridsuite.merge.orchestrator.server.repositories.TsoEntity;
 
 /**
  * @author Chamseddine Benhamed <chamseddine.benhamed at rte-france.com>
  */
 @AllArgsConstructor
 @Getter
-@Builder
-@ToString
 @ApiModel("Tso")
 public class Tso {
     private String sourcingActor;
     private String alternativeSourcingActor;
-
-    public Tso(TsoEntity tsoEntity) {
-        this.sourcingActor = tsoEntity.getSourcingActor();
-        this.alternativeSourcingActor = tsoEntity.getAlternativeSourcingActor();
-    }
 }
