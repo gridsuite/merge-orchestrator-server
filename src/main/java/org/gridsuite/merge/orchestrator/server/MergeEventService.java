ce3d3755eccf592c2b511157a93e44b371fe675e
Seddik Yengui <seddik.yengui@rte-france.com>
2022-09-15 17:00:23 +0200

    centralization of notifications

--- src/main/java/org/gridsuite/merge/orchestrator/server/MergeEventService.java
+++ src/main/java/org/gridsuite/merge/orchestrator/server/MergeEventService.java
@@ -6,22 +6,22 @@
  */
 package org.gridsuite.merge.orchestrator.server;
 
+import org.gridsuite.merge.orchestrator.server.dto.IgmStatus;
+import org.gridsuite.merge.orchestrator.server.dto.MergeStatus;
+import org.gridsuite.merge.orchestrator.server.repositories.IgmEntity;
+import org.gridsuite.merge.orchestrator.server.repositories.IgmEntityKey;
+import org.gridsuite.merge.orchestrator.server.repositories.IgmRepository;
+import org.gridsuite.merge.orchestrator.server.repositories.MergeEntity;
+import org.gridsuite.merge.orchestrator.server.repositories.MergeEntityKey;
+import org.gridsuite.merge.orchestrator.server.repositories.MergeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.UUID;
-
-import org.gridsuite.merge.orchestrator.server.dto.IgmStatus;
-import org.gridsuite.merge.orchestrator.server.dto.MergeStatus;
-import org.gridsuite.merge.orchestrator.server.repositories.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.stream.function.StreamBridge;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.stereotype.Service;
 
 /**
  * @author Jon Harper <jon.harper at rte-france.com>
@@ -34,15 +34,8 @@ public class MergeEventService {
 
     private IgmRepository igmRepository;
 
-    private static final String CATEGORY_BROKER_OUTPUT = MergeEventService.class.getName() + ".output-broker-messages";
-
-    private static final String PROCESS_UUID = "processUuid";
-    private static final String BUSINESS_PROCESS = "businessProcess";
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(CATEGORY_BROKER_OUTPUT);
-
     @Autowired
-    private StreamBridge mergeInfosPublisher;
+    private NotificationService notificationService;
 
     public MergeEventService(MergeRepository mergeRepository, IgmRepository igmRepository) {
         this.mergeRepository = mergeRepository;
@@ -61,15 +54,11 @@ public class MergeEventService {
         igmRepository.save(new IgmEntity(new IgmEntityKey(processUuid, localDateTime, tso), status.name(), networkUuid, caseUuid,
                 localReplacingDateTime, replacingBusinessProcess, eqBoundary, tpBoundary));
 
-        Message<String> message = MessageBuilder
-                .withPayload("")
-                .setHeader(PROCESS_UUID, processUuid)
-                .setHeader(BUSINESS_PROCESS, businessProcess)
-                .setHeader("date", date.format(DateTimeFormatter.ISO_DATE_TIME))
-                .setHeader("tso", tso)
-                .setHeader("status", status.name())
-                .build();
-        sendMergeMessage(message);
+        notificationService.emitMergeIgmEvent(processUuid,
+                businessProcess,
+                date.format(DateTimeFormatter.ISO_DATE_TIME),
+                tso,
+                status.name());
     }
 
     public void addMergeEvent(UUID processUuid, String businessProcess, ZonedDateTime date, MergeStatus status) {
@@ -77,29 +66,10 @@ public class MergeEventService {
         var mergeEntity = getOrCreateMergeEntity(processUuid, date);
         mergeEntity.setStatus(status.name());
         mergeRepository.save(mergeEntity);
-        Message<String> message = MessageBuilder
-                .withPayload("")
-                .setHeader(PROCESS_UUID, processUuid)
-                .setHeader(BUSINESS_PROCESS, businessProcess)
-                .setHeader("date", date.format(DateTimeFormatter.ISO_DATE_TIME))
-                .setHeader("status", status.name())
-                .build();
-        sendMergeMessage(message);
-    }
-
-    private void sendMergeMessage(Message<String> message) {
-        LOGGER.debug("Sending message : {}", message);
-        mergeInfosPublisher.send("publishMerge-out-0", message);
-    }
-
-    public void addErrorEvent(UUID processUuid, String businessProcess, String errorMessage) {
-        Message<String> message = MessageBuilder
-            .withPayload("")
-            .setHeader(PROCESS_UUID, processUuid)
-            .setHeader(BUSINESS_PROCESS, businessProcess)
-            .setHeader("error", errorMessage)
-            .build();
-        sendMergeMessage(message);
+        notificationService.emitMergeEvent(processUuid,
+                businessProcess,
+                date.format(DateTimeFormatter.ISO_DATE_TIME),
+                status.name());
     }
 
     MergeEntity getOrCreateMergeEntity(UUID processUuid, ZonedDateTime date) {
---
+++ src/main/java/org/gridsuite/merge/orchestrator/server/NotificationService.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.gridsuite.merge.orchestrator.server;
+
+import org.gridsuite.merge.orchestrator.server.utils.annotations.PostCompletion;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+/**
+ * @author Seddik Yengui <seddik.yengui at rte-france.com
+ */
+
+@Service
+public class NotificationService {
+
+    private static final String CATEGORY_BROKER_OUTPUT = MergeEventService.class.getName() + ".output-broker-messages";
+
+    private static final String PROCESS_UUID_HEADER = "processUuid";
+    private static final String BUSINESS_PROCESS_HEADER = "businessProcess";
+    private static final String DATE_HEADER = "date";
+    private static final String STATUS_HEADER = "status";
+    private static final String TSO_HEADER = "tso";
+    private static final String ERROR_HEADER = "error";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CATEGORY_BROKER_OUTPUT);
+
+    @Autowired
+    private StreamBridge mergeInfosPublisher;
+
+    private void sendMergeMessage(Message<String> message) {
+        LOGGER.debug("Sending message : {}", message);
+        mergeInfosPublisher.send("publishMerge-out-0", message);
+    }
+
+    @PostCompletion
+    public void emitMergeIgmEvent(UUID processUuid, String businessProcess, String date, String tso, String status) {
+        Message<String> message = MessageBuilder
+                .withPayload("")
+                .setHeader(PROCESS_UUID_HEADER, processUuid)
+                .setHeader(BUSINESS_PROCESS_HEADER, businessProcess)
+                .setHeader(DATE_HEADER, date)
+                .setHeader(TSO_HEADER, tso)
+                .setHeader(STATUS_HEADER, status)
+                .build();
+        sendMergeMessage(message);
+    }
+
+    @PostCompletion
+    public void emitMergeEvent(UUID processUuid, String businessProcess, String date, String status) {
+        Message<String> message = MessageBuilder
+                .withPayload("")
+                .setHeader(PROCESS_UUID_HEADER, processUuid)
+                .setHeader(BUSINESS_PROCESS_HEADER, businessProcess)
+                .setHeader(DATE_HEADER, date)
+                .setHeader(STATUS_HEADER, status)
+                .build();
+        sendMergeMessage(message);
+    }
+
+    @PostCompletion
+    public void emitErrorEvent(UUID processUuid, String businessProcess, String errorMessage) {
+        Message<String> message = MessageBuilder
+                .withPayload("")
+                .setHeader(PROCESS_UUID_HEADER, processUuid)
+                .setHeader(BUSINESS_PROCESS_HEADER, businessProcess)
+                .setHeader(ERROR_HEADER, errorMessage)
+                .build();
+        sendMergeMessage(message);
+    }
+}
